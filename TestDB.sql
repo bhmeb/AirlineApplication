1610d314273a2ff5bbbbf1d3a17c3c03b75be269
harishmsgit <senharishms108@gmail.com>
2024-02-25 01:54:59 +0530

    airline application test data and response data generated

---
+++ TestDB.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM airlinedb.flight
+LIMIT 0, 1000
+
+-- Date: 2024-02-25 01:53
+*/
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1001,'Kolkata','CCU','45 Minutes','Mumbai','BOM');
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1002,'Mumbai','BOM','30 Minutes','Kolkata','CCU');
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1003,'Mumbai','BOM','55 Minutes','Delhi','DEL');
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1004,'Mumbai','BOM','60 Minutes','Bangalore ','BLR');
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1005,'Mumbai','BOM','25 Minutes','Bhubaneswar ','BBI');
+INSERT INTO `` (`flight_number`,`dest_airport`,`dest_code`,`duration`,`org_airport`,`org_code`) VALUES (1006,'Kolhapur','KLH','35 Minutes','Kochi ','COK');
